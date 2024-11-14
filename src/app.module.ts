377ae4307516f9ee582ab48b041c849fef3dc79c
june <ryu9663@naver.com>
2024-11-14 21:41:33 +0900

    feat: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from 'src/configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
   controllers: [UsersController],
 })
 export class AppModule {}
